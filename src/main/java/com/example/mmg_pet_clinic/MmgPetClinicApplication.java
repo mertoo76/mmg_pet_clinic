efdf2af3d56fdbdb7c451cc01410034c0493cc03
mertoo76 <mertgun76@gmail.com>
2020-02-18 21:58:34 +0300

    initial commit

---
+++ src/main/java/com/example/mmg_pet_clinic/MmgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmg_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmgPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mmg_pet_clinic/MmgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmg_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
