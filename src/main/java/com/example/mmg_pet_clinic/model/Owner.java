74cc0be31200cb4b9e550288fdd047f9f95b1562
mertoo76 <mertgun76@gmail.com>
2020-02-21 16:50:24 +0200

    Adding POJOs. Closes #2

---
+++ src/main/java/com/example/mmg_pet_clinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.example.mmg_pet_clinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/com/example/mmg_pet_clinic/model/Vet.java
@@ -0,0 +1,7 @@
+package com.example.mmg_pet_clinic.model;
+
+public class Vet extends Person {
+
+	
+
+}
